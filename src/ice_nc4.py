4c2e057bc4a76b28f3ab838923fa7db6163dfae0
Davide Marchegiani <davide.marchegiani@gmail.com>
2025-01-28 15:26:56 +1100

    Updated ice_nc4.py: - Separate monthly and daily processing - Only perform processing if respective data exist

--- src/ice_nc4.py
+++ src/ice_nc4.py
@@ -8,53 +8,57 @@
 
 # Should be run in the CICE history output directory
 
-import os, glob, re, subprocess, calendar
+import os, glob, re, subprocess, warnings
+from collections import defaultdict
+from calendar import monthrange
 
 # Expect that all files have prefix iceh
 
 # Check for end of string so as not to match nc4 files etc.
 # Rename to iceh_m and iceh_d so that they don't match.
-
 verbose = True
-re_monthly = re.compile("iceh.\d\d\d\d-\d\d.nc")
-re_daily = re.compile("iceh.\d\d\d\d-\d\d-\d\d.nc")
+re_monthly = re.compile(r"iceh\.\d{4}-\d{2}\.nc")
+re_daily = re.compile(r"iceh\.\d{4}-\d{2}-\d{2}\.nc")
+
 monthly = []
+daily = []
 for f in glob.glob('iceh.*.nc'):
-    m = re_monthly.match(f)
-    if m:
+    if re_monthly.match(f):
         monthly.append(f)
-        continue
-    m = re_daily.match(f)
-    if m:
-        continue
-    raise Exception("Unexpected file %s" % f)
+    elif re_daily.match(f):
+        daily.append(f)
+    else:
+        warnings.warn(f"Unexpected file '{f}'.")
 
-if not monthly:
-    print("No files to process")
+# Convert monthly data to netCDF4 format and compress
+if monthly:
+    for f in monthly:
+        cmd = ["nccopy", "-k3", "-d4", f, f"iceh_m{f[4:]}"]
+        if verbose:
+            print(cmd)
+        subprocess.check_call(cmd, stderr=subprocess.STDOUT)
+else:
+    print("No monthly data to process.")
 
-monthly.sort()
-
-cal = calendar.Calendar()
-for f in monthly:
-    # Note that spaces are not allowed in subprocess arguments
-    cmd = ["nccopy", "-k3", "-d4", f, "iceh_m%s" % f[4:]]
-    if verbose:
-        print(cmd)
-    subprocess.check_call(cmd, stderr=subprocess.STDOUT)
-
-    year = int(f[5:9])
-    month = int(f[10:12])
-
-    cmd = ["ncrcat", "-4", "--deflate", "4"]
-    for date in cal.itermonthdates(year,month):
-        # Days of this month (assuming proleptic Gregorian)
-        # This returns days of preceding and following months
-        # to give complete weeks, so need to check actual month.
-        if date.month == month:
-            # strftime doesn't format years before 1000 properly
-            cmd.append("iceh.%4.4d-%2.2d-%2.2d.nc" %
-                       (date.year, date.month, date.day))
-    cmd.append("iceh_d%s" % f[4:])
-    if verbose:
-        print(cmd)
-    subprocess.check_call(cmd, stderr=subprocess.STDOUT)
+# Combine separate daily data in the same netCDF4 file
+if daily:
+    daily.sort()
+    # Group files with same year and month
+    grouped = defaultdict(list)
+    for filename in daily:
+        year, month, _ = filename[5:-3].split("-")
+        grouped[(year, month)].append(filename)
+    
+    # Combine data if all days are present for each year-month group
+    for (year, month), files in grouped.items():
+        days_in_month = monthrange(int(year), int(month))[1]
+        available_days = [int(f[-5:-3]) for f in files]
+        if all(day in available_days for day in range(1, days_in_month + 1)):
+            cmd = ["ncrcat", "-4", "--deflate", "4", *files, f"iceh_d.{year}-{month}.nc"]
+            if verbose:
+                print(cmd)
+            subprocess.check_call(cmd, stderr=subprocess.STDOUT)
+        else:
+            raise Exception(f"Missing daily data for {year}-{month}. Available files: {files}")
+else:
+    print("No daily data to process.")
